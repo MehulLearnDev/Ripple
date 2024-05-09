8f902ed6b8c082cbcaf7767dff624899765fe924
AWPtimizer <luhemdha@gmail.com>
2024-05-09 13:09:27 +0530

    Add Controllers

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import {v2 as cloudinary} from 'cloudinary'
 
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js"
+
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -16,9 +25,9 @@ app.use(express.urlencoded({ extended: true })); // to parse form data(urlencode
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on Port: ${PORT}`);
   connectMongoDB();
 });
-// 1:7:39
