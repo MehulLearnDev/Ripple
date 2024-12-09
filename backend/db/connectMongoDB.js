393381eb5b499a0dcaa97576622aed3ebb242986
MehulLearnDev <mehul.learndev@gmail.com>
2024-12-09 09:23:16 +0530

    Update connectMongoDB.js

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGODB_URI);
+    const connection = await mongoose.connect("mongodb+srv://mehul:Mehul%400707@cluster0.oak8e2r.mongodb.net/talk-shit-db");
     console.log(`MongoDB connected: ${connection.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
